fb10d544a861caa5aaaef189bf4e0fed47436457
s188691 <wiktor.wnekowicz@gmail.com>
2021-03-25 14:03:42 +0100

    All functionalities ready

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+    public Animator animator;
+
+    public void ShowPopUp()
+    {
+        animator.Play("PopUpAnimShow");
+    }
+
+    public void HidePopUp()
+    {
+        animator.Play("PopUpAnimHide");
+    }
+}
